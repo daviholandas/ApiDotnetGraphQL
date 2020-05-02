a5f05987b51140d117b5f5813db48417eb5bc370
Davi Holanda <daviholandas@gmail.com>
2020-05-01 23:20:44 -0300

    Initial

---
+++ Entities/Product.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ApiGraph.Entities
+{
+    public class Product : Entity
+    {
+        [Required]
+        [StringLength(200)]
+        public string Name {get; set;}
+        
+        [Column(TypeName="decimal(18,2)")]
+        public decimal Price {get; set;}
+        public Category Category {get; set;}
+        public string Description {get; set;}
+        public DateTime DateRegister {get; set;}
+
+        //EF Relations
+        [JsonIgnore]
+        public Guid CategoryId {get; set;}
+    }
+}
